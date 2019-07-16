2c7ce059785d8de60ab7ed1acbe4e786027f2b67
Sandrine Patin <sandrine.patin@aviation-civile.gouv.fr>
2019-07-16 13:59:11 +0200

    Display calendar

--- php/classes/Calendar.php
+++ php/classes/Calendar.php
@@ -9,13 +9,38 @@ class Calendar
 
     public function __construct($m = null, $y = null)
     {
-        $this->month=$m;
-        $this->year=$y;
+        $this->month = $m;
+        $this->year = $y;
     }
 
-    public function displayCalendar(){
-        
-        echo "ceci est un calendrier du jour";
+    public function displayCalendar()
+    {
+        $numberDays = cal_days_in_month(CAL_GREGORIAN, $this->month, $this->year);
+        echo $numberDays;
+
+        $row = "<table class=\"uk-table\">
+                    <caption></caption>
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <td></td>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            ";
+
+        for ($i = 1; $i <= $numberDays; $i++) {
+            $row .= "<tr><td>" . $i . "-" . $this->month . "-" . $this->year . "</td></tr>";
+        }
+
+        $row .= "</tbody>
+        </table>";
+
+        echo $row;
     }
 
 }
